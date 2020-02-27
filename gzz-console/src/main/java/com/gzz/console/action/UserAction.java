87c883055c3e049b56bce44b9cc5d6c038ef24fb
Gm-X1 <hi.gmdeng@gmail.com>
2020-02-27 22:58:29 +0800

    1增加数据验证 2，重复提交

--- gzz-console/src/main/java/com/gzz/console/action/UserAction.java
+++ gzz-console/src/main/java/com/gzz/console/action/UserAction.java
@@ -9,11 +9,12 @@ package com.gzz.console.action;
  * <author>          <time>          <version>          <desc>
  * 作者姓名           修改时间           版本号              描述
  */
+
 import com.gzz.console.service.UserService;
 import com.gzz.console.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 〈一句话功能简述〉<br>
@@ -23,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @create 2018/6/26
  * @since 1.0.0
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserAction {
 
@@ -31,7 +32,7 @@ public class UserAction {
     private UserService userService;
 
     @RequestMapping("/info")
-    public UserVo info(){
+    public UserVo info(String name){
         return userService.getUser();
     }
 }
